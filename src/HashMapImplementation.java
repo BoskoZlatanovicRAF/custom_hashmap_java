e1ecdd6860711357ac42b619f0afdbc7d39401ad
Bosko Zlatanovic <bzlatanovic3621rn@raf.rs>
2025-04-05 02:58:34 +0200

    removed size field

--- src/HashMapImplementation.java
+++ src/HashMapImplementation.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class HashMapImplementation<K, V> implements Iterable<HashMapImplementation<K,V>.Entry> {
 
@@ -23,40 +20,31 @@ public class HashMapImplementation<K, V> implements Iterable<HashMapImplementati
     }
 
 
-    private int size;
     private int currentSize;
-    private final double loadFactor = 0.75;
-    private final List<List<Entry>> hashMap;
+    private final static double LOAD_FACTOR = 0.75;
+    private final List<List<Entry>> buckets;
 
     public HashMapImplementation() {
-        this.size = 16;
-        this.hashMap = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            this.hashMap.add(null);
-        }
+        this.buckets = new ArrayList<>(Collections.nCopies(4, null));
     }
 
     public HashMapImplementation(int initialSize) {
-        this.size = initialSize;
-        this.hashMap = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            this.hashMap.add(null);
-        }
+        this.buckets = new ArrayList<>(Collections.nCopies(initialSize, null));
     }
 
     public void put(K key, V value) {
-        if((double) currentSize / size >= loadFactor){
+        if((double) currentSize / buckets.size() >= LOAD_FACTOR){
             resize();
         }
 
         Entry entry = new Entry(key, value);
-        int index = hash(entry.getKey(), this.size);
+        int index = hash(entry.getKey(), buckets.size());
 
-        if (hashMap.get(index) == null) {
-            hashMap.set(index, new ArrayList<>());
+        if (buckets.get(index) == null) {
+            buckets.set(index, new ArrayList<>());
         }
 
-        List<Entry> bucket = hashMap.get(index);
+        List<Entry> bucket = buckets.get(index);
 
         for (Entry e : bucket) {
             if (Objects.equals(e.getKey(), entry.getKey())) {
@@ -71,13 +59,13 @@ public class HashMapImplementation<K, V> implements Iterable<HashMapImplementati
 
 
     public void remove(K key) {
-        int index = hash(key, this.size);
+        int index = hash(key, buckets.size());
 
-        if (hashMap.get(index) == null) {
+        if (buckets.get(index) == null) {
             return;
         }
 
-        List<Entry> entries = hashMap.get(index);
+        List<Entry> entries = buckets.get(index);
         Iterator<Entry> iterator = entries.iterator();
 
         while(iterator.hasNext()) {
@@ -91,13 +79,13 @@ public class HashMapImplementation<K, V> implements Iterable<HashMapImplementati
     }
 
     public V get(K key) {
-        int index = hash(key, this.size);
+        int index = hash(key, buckets.size());
 
-        if(hashMap.get(index) == null){
+        if(buckets.get(index) == null){
             return null;
         }
 
-        List<Entry> entries = hashMap.get(index);
+        List<Entry> entries = buckets.get(index);
 
         for(Entry entry: entries){
             if(Objects.equals(entry.getKey(), key)){ // null safe
@@ -109,13 +97,13 @@ public class HashMapImplementation<K, V> implements Iterable<HashMapImplementati
     }
 
     private void resize() {
-        int newSize = size * 2;
+        int newSize = buckets.size() * 2; //4 -> 8
         List<List<Entry>> newHashMap = new ArrayList<>(newSize);
         for (int i = 0; i < newSize; i++) {
             newHashMap.add(null);
         }
 
-        for (List<Entry> bucket : hashMap) {
+        for (List<Entry> bucket : buckets) {
             if (bucket != null) {
                 for (Entry entry : bucket) {
                     int newIndex = hash(entry.getKey(), newSize);
@@ -129,15 +117,14 @@ public class HashMapImplementation<K, V> implements Iterable<HashMapImplementati
             }
         }
 
-        this.hashMap.clear();
-        this.hashMap.addAll(newHashMap);
-        this.size = newSize;
+        this.buckets.clear();
+        this.buckets.addAll(newHashMap);
     }
 
     @Override
     public Iterator<Entry> iterator() {
         List<Entry> entries = new ArrayList<>();
-        for (List<Entry> bucket : hashMap) {
+        for (List<Entry> bucket : buckets) {
             if (bucket != null) {
                 entries.addAll(bucket);
             }
@@ -148,6 +135,7 @@ public class HashMapImplementation<K, V> implements Iterable<HashMapImplementati
     private int hash(K key, int size){
         String keyString = String.valueOf(key);
         byte[] bytes = keyString.getBytes();
+
         int hash = 0;
         for(byte b : bytes){
             hash = (31 * hash + b) % size;
@@ -155,7 +143,6 @@ public class HashMapImplementation<K, V> implements Iterable<HashMapImplementati
         return Math.abs(hash);
     }
 
-    public int currentSize() {
-        return this.currentSize;
-    }
+    public int currentSize() { return this.currentSize; }
+
 }
